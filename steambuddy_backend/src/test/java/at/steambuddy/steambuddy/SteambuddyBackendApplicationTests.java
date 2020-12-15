d2258691e6ef6885e9efe4a59aa3ec53ee83b8f0
Philipp Starzacher <p.starzacher@gmail.com>
2020-12-15 20:48:57 +0100

    Initial Backend Setup with simple RestController

---
+++ steambuddy_backend/src/test/java/at/steambuddy/steambuddy/SteambuddyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package at.steambuddy.steambuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteambuddyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
