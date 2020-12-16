e35568a2fb2cb095d429e2a5dcb3c0f221802285
Peter Jochum <peter.jochum@aau.at>
2020-12-16 16:32:34 +0100

    Added initial games list

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GamesListComponent } from './games-list/games-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GamesListComponent
   ],
   imports: [
     BrowserModule,
