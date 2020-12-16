70d9f259b4313c8524c6a68fa177cf602cbadf38
Philipp Starzacher <p.starzacher@gmail.com>
2020-12-16 18:10:51 +0100

    Added Swagger Annotations

--- steambuddy-parent/steambuddy-api/src/main/java/org/steambuddy/api/endpoints/GameRetrievalEndpoint.java
+++ steambuddy-parent/steambuddy-api/src/main/java/org/steambuddy/api/endpoints/GameRetrievalEndpoint.java
@@ -7,9 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.steambuddy.api.models.Game;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(value = "GameResources")
 @RestController
 public class GameRetrievalEndpoint {
 
+	@ApiOperation(value = "Get the list of all games.", nickname = "games", notes = "Returns all games.", tags = "GameResources", response = Game.class, responseContainer = "List")
 	@GetMapping("/games")
 	public List<Game> getGames() {
 		List<Game> games = new ArrayList<Game>();
