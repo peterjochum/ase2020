0a201393a0e44dc2f6b7fb83d96de85f2b97ab56
Philipp Starzacher <p.starzacher@gmail.com>
2020-12-16 17:06:32 +0100

    Added main datamodels

---
+++ steambuddy-parent/steambuddy-api/src/main/java/org/steambuddy/api/models/Games.java
@@ -0,0 +1,55 @@
+package org.steambuddy.api.models;
+
+public class Games {
+
+	private Long id;
+
+	private String name;
+
+	private int year;
+
+	private String image;
+
+	private String text;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+}
