f0d3f3d21db4503308f8ec3315d468a4ea96f31a
Philipp Starzacher <p.starzacher@gmail.com>
2020-12-27 14:46:27 +0100

    Added JPA Annotations for GameEntity and configured spring to use a datasource

--- steambuddy-parent/steambuddy-app/src/main/java/org/steambuddy/app/config/BackendConfiguration.java
+++ steambuddy-parent/steambuddy-app/src/main/java/org/steambuddy/app/config/BackendConfiguration.java
@@ -2,7 +2,6 @@ package org.steambuddy.app.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.steambuddy.app.service.GameService;
 
 @Configuration
 @ComponentScan
