1f3d0cb84b5f70a77d740675eb044bc84b6d3b96
Florian Zenzmaier <F.Zenzmaier@gmail.com>
2021-01-25 10:28:15 +0100

    Update GameService.java

--- steambuddy-parent/steambuddy-app/src/main/java/org/steambuddy/app/service/GameService.java
+++ steambuddy-parent/steambuddy-app/src/main/java/org/steambuddy/app/service/GameService.java
@@ -2,16 +2,23 @@ package org.steambuddy.app.service;
 
 import java.util.List;
 
+import org.steambuddy.api.dto.GameCollectionDTO;
 import org.steambuddy.api.dto.GameDTO;
 import org.steambuddy.api.dto.UserDTO;
 
 public interface GameService {
 
 	List<GameDTO> getGames();
+
 	List<GameDTO> getGames(String name);
 	
 	List<GameDTO> getGameSuggestions(UserDTO user);
 
 	GameDTO getGame(Long id);
 
+	GameCollectionDTO getGameCollectionByUserId(Long userId);
+
+	GameCollectionDTO addGameToGameCollection(Long gameCollection, Long gameId);
+
+	GameCollectionDTO removeGameFromGameCollection(Long userId, Long gameId);
 }
