2a5283bb2d6a760bc4aa4c02fa6c6e0818181b71
Florian Zenzmaier <F.Zenzmaier@gmail.com>
2021-05-11 06:38:27 +0200

    Update Tests

--- steambuddy-parent/steambuddy-app/src/test/java/org/steambuddy/app/test/RepoTest.java
+++ steambuddy-parent/steambuddy-app/src/test/java/org/steambuddy/app/test/RepoTest.java
@@ -10,7 +10,10 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.steambuddy.app.compositekeys.GameRatingKey;
+import org.steambuddy.app.compositekeys.MessageKey;
+import org.steambuddy.app.entity.MessageEntity;
 import org.steambuddy.app.entity.RatingEntity;
+import org.steambuddy.app.repository.MessageRepository;
 import org.steambuddy.app.repository.RatingRepository;
 
 import java.util.ArrayList;
@@ -26,9 +29,10 @@ import static org.junit.Assert.assertEquals;
 
 public class RepoTest {
     @Autowired RatingRepository repository;
+    @Autowired MessageRepository messageRepo;
     //@Autowired TestEntityManager testEntityManager;
     @Test
-    public void repoCorrectlySavesRating() {
+    public void ratingRepoCorrectlySavesRating() {
         GameRatingKey key = new GameRatingKey(10L,10L);
         RatingEntity rating = new RatingEntity(key,5L,"Test");
         rating=repository.save(rating);
@@ -38,7 +42,7 @@ public class RepoTest {
     }
     
     @Test
-    public void repoCorrectlyRemovesRating() {
+    public void ratingRepoCorrectlyRemovesRating() {
         GameRatingKey key = new GameRatingKey(11L,11L);
         RatingEntity rating = new RatingEntity(key,5L,"Test");
         rating=repository.save(rating);
@@ -49,40 +53,28 @@ public class RepoTest {
         assertEquals(emptyRating,Optional.empty());
     }
     
-    /*
     @Test
-    public void repoCorrectlyFindsSongById() {
-        List<Song> songs = insertFourSongsInDataBase();
-        int testSongIndex = 3;
-        SongId idToRetrieve = songs.get(testSongIndex).getId();
-        Song result = repository.findById(idToRetrieve).orElseThrow(SongNotFoundException::new);
-        assertTrue(result.getId().getAlbum().equals(songs.get(testSongIndex).getId().getAlbum()));
-        assertTrue(result.getId().getName().equals(songs.get(testSongIndex).getId().getName()));
-        assertTrue(result.getId().getArtist().equals(songs.get(testSongIndex).getId().getArtist()));
-        assertTrue(result.getDuration() == songs.get(testSongIndex).getDuration());
-        assertTrue(result.getRating() == songs.get(testSongIndex).getRating());
-        assertTrue(result.getDownloadUrl().equals(songs.get(testSongIndex).getDownloadUrl()));
-    }
-    private List<Song> insertFourSongsInDataBase() {
-        SongId songId1 = new SongId("test_name1", "test_album1", "test_artist1");
-        Song song1 = new Song(songId1, 23, null, null, 3, "http://download.this.song1");
-        SongId songId2 = new SongId("test_name2", "test_album2", "test_artist2");
-        Song song2 = new Song(songId2, 21, null, null, 2, "http://download.this.song2");
-        SongId songId3 = new SongId("test_name3", "test_album3", "test_artist3");
-        Song song3 = new Song(songId3, 20, null, null, 4, "http://download.this.song3");
-        SongId songId4 = new SongId("test_name4", "test_album4", "test_artist4");
-        Song song4 = new Song(songId4, 26, null, null, 2, "http://download.this.song4");
-        List<Song> songs = new ArrayList<>();
-        songs.add(song1);
-        songs.add(song2);
-        songs.add(song3);
-        songs.add(song4);
-        testEntityManager.persistAndFlush(song1);
-        testEntityManager.persistAndFlush(song2);
-        testEntityManager.persistAndFlush(song3);
-        testEntityManager.persistAndFlush(song4);
-        return songs;
+    public void messageRepoCorrectlySavesMessages() {
+        MessageKey key = new MessageKey(10L,10L,1000L);
+        MessageEntity message = new MessageEntity(key,"This is my message");
+        message=messageRepo.save(message);
+        
+        MessageEntity messageFromR=messageRepo.findById(key).get();
+        assertTrue(key.equals(messageFromR.getMessageKey()) && message.getMessage().equals(messageFromR.getMessage()));
+    }    
+
+    @Test
+    public void messageRepoCorrectlyRemovesRating() {
+        MessageKey key = new MessageKey(10L,10L,1000L);
+        MessageEntity message = new MessageEntity(key,"This is my message");
+        message=messageRepo.save(message);
+        
+        MessageEntity messageFromR=messageRepo.findById(key).get();
+        assertTrue(key.equals(messageFromR.getMessageKey()) && message.getMessage().equals(messageFromR.getMessage()));
+        
+        messageRepo.delete(message);
+        Optional<MessageEntity> emptyMessage=messageRepo.findById(key);
+        assertEquals(emptyMessage,Optional.empty());
     }
     
-    */
 }
